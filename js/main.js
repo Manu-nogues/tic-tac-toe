4a8620343a98609dc7fd500127b2ac19c98cb413
Manu-nogues <girstyle.n.m@gmail.com>
2022-10-14 19:05:51 +0200

    primera parte

---
+++ js/main.js
@@ -0,0 +1,131 @@
+// const X_CLASS = 'x'
+// const CIRCLE_CLASS = 'circle'
+// const WINNING_COMBINATIONS = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6]
+// ]
+// const cellElements = document.querySelectorAll('[data-cell]')
+// const board = document.getElementById('board')
+// const winningMessageElement = document.getElementById('winningMessage')
+// const restartButton = document.getElementById('restartButton')
+// const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+// let circleTurn
+// let turnos = 6
+
+// startGame()
+
+// restartButton.addEventListener('click', startGame)
+
+// function startGame() {
+//   circleTurn = false
+//   cellElements.forEach(cell => {
+//     cell.classList.remove(X_CLASS)
+//     cell.classList.remove(CIRCLE_CLASS)
+//     cell.removeEventListener('click', handleClick)
+//     cell.addEventListener('click', handleClick, { once: true })
+//   })
+//   setBoardHoverClass()
+//   winningMessageElement.classList.remove('show')
+// }
+
+// function handleClick(e) {
+//   const cell = e.target
+//   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+//   placeMark(cell, currentClass)
+//   if (checkWin(currentClass)) {
+//     endGame(false)
+//   } else if (isDraw()) {
+//     endGame(true)
+//   } else {
+//     swapTurns()
+//     setBoardHoverClass()
+//   }
+// }
+
+// function endGame(draw) {
+//   if (draw) {
+//     winningMessageTextElement.innerText = 'Draw!'
+//   } else {
+//     winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+//   }
+//   winningMessageElement.classList.add('show')
+// }
+
+// function isDraw() {
+//   return [...cellElements].every(cell => {
+//     return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+//   })
+// }
+
+// function placeMark(cell, currentClass) {
+//   if (turnos <= 0){
+//     console.log("adios")
+//   } else {
+//     cell.classList.add(currentClass)
+//   turnos--
+//   console.log("hola")
+// }
+
+// }
+
+// function swapTurns() {
+//   circleTurn = !circleTurn
+// }
+
+// function setBoardHoverClass() {
+//   board.classList.remove(X_CLASS)
+//   board.classList.remove(CIRCLE_CLASS)
+//   if (circleTurn) {
+//     board.classList.add(CIRCLE_CLASS)
+//   } else {
+//     board.classList.add(X_CLASS)
+//   }
+// }
+
+// function checkWin(currentClass) {
+//   return WINNING_COMBINATIONS.some(combination => {
+//     return combination.every(index => {
+//       return cellElements[index].classList.contains(currentClass)
+//     })
+//   })
+// }
+
+let turnos = 6
+
+
+
+
+let interruptor = true;
+let casillas = Array.from(document.getElementsByClassName("cell"))
+casillas.map((casilla, /*index*/) => {
+    casilla.addEventListener("click", () => {
+        if (turnos > 0) {
+            if (casilla.innerHTML == "") {
+                casilla.innerHTML = (interruptor) ? "X" : "O"
+                interruptor = !interruptor;
+                turnos = turnos - 1
+                console.log(turnos)
+
+            }
+        } else {
+            (casilla.innerHTML = "")
+        }
+
+
+    });
+});
+
+function reset() {
+    document.querySelectorAll(".cell").forEach((casilla) => {
+        casilla.innerHTML = "";
+    });
+}
+
+
+
