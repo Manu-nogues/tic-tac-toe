d6971ca5ef6f79032caef97341736c255e0ff44e
Manu-nogues <114058678+Manu-nogues@users.noreply.github.com>
2022-10-16 15:49:42 +0200

    Seleccion de jugadores

--- js/main.js
+++ js/main.js
@@ -1,106 +1,28 @@
-// const X_CLASS = 'x'
-// const CIRCLE_CLASS = 'circle'
-// const WINNING_COMBINATIONS = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [2, 4, 6]
-// ]
-// const cellElements = document.querySelectorAll('[data-cell]')
-// const board = document.getElementById('board')
-// const winningMessageElement = document.getElementById('winningMessage')
-// const restartButton = document.getElementById('restartButton')
-// const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-// let circleTurn
-// let turnos = 6
 
-// startGame()
+///ARRAY notificador de quien ha ganado///
 
-// restartButton.addEventListener('click', startGame)
-
-// function startGame() {
-//   circleTurn = false
-//   cellElements.forEach(cell => {
-//     cell.classList.remove(X_CLASS)
-//     cell.classList.remove(CIRCLE_CLASS)
-//     cell.removeEventListener('click', handleClick)
-//     cell.addEventListener('click', handleClick, { once: true })
-//   })
-//   setBoardHoverClass()
-//   winningMessageElement.classList.remove('show')
-// }
-
-// function handleClick(e) {
-//   const cell = e.target
-//   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-//   placeMark(cell, currentClass)
-//   if (checkWin(currentClass)) {
-//     endGame(false)
-//   } else if (isDraw()) {
-//     endGame(true)
-//   } else {
-//     swapTurns()
-//     setBoardHoverClass()
-//   }
-// }
-
-// function endGame(draw) {
-//   if (draw) {
-//     winningMessageTextElement.innerText = 'Draw!'
-//   } else {
-//     winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-//   }
-//   winningMessageElement.classList.add('show')
-// }
-
-// function isDraw() {
-//   return [...cellElements].every(cell => {
-//     return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-//   })
-// }
-
-// function placeMark(cell, currentClass) {
-//   if (turnos <= 0){
-//     console.log("adios")
-//   } else {
-//     cell.classList.add(currentClass)
-//   turnos--
-//   console.log("hola")
-// }
-
-// }
-
-// function swapTurns() {
-//   circleTurn = !circleTurn
-// }
-
-// function setBoardHoverClass() {
-//   board.classList.remove(X_CLASS)
-//   board.classList.remove(CIRCLE_CLASS)
-//   if (circleTurn) {
-//     board.classList.add(CIRCLE_CLASS)
-//   } else {
-//     board.classList.add(X_CLASS)
-//   }
-// }
-
-// function checkWin(currentClass) {
-//   return WINNING_COMBINATIONS.some(combination => {
-//     return combination.every(index => {
-//       return cellElements[index].classList.contains(currentClass)
-//     })
-//   })
-// }
+const STATUS_DISPLAY = document.querySelector('.game-notification'),
+    GAME_STATE = ["", "", "", "", "", "", "", "", ""],
+    WINNINGS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ],
+    WIN_MESSAGE = () => `El jugador ${casilla} ha ganado!`,
+    DRAW_MESSAGE = () => `El juego ha terminado en empate!`,
+    CURRENT_PLAYER_TURN = () => `Turno del jugador ${casilla}`
 
 
 
+
+///Funcion para escribir y sobre escribir el tablero///
 
 let turnos = 6
-    
 let interruptor = true;
 let casillas = Array.from(document.getElementsByClassName("cell"))
 casillas.map((casilla, /*index*/) => {
@@ -116,18 +38,21 @@ casillas.map((casilla, /*index*/) => {
 
 
             }
+
+            ///funcion para poder mover las las fichas de sitio///
+
         } else {
-            if(casilla.innerHTML == "X") {
+            if (casilla.innerHTML == "X") {
                 interruptor = true;
                 (casilla.innerHTML = "")
                 turnos = turnos + 1
-            } else if(casilla.innerHTML == "O") {
+            } else if (casilla.innerHTML == "O") {
                 interruptor = false;
                 (casilla.innerHTML = "")
                 turnos = turnos + 1
             }
         }
-    
+
         console.log(pepito)
 
     });
---
+++ js/seleccion.js
@@ -0,0 +1,27 @@
+function guardarInput (){
+    let input=document.getElementById('pepito1');
+    let inputt=document.getElementById('pepito2');
+    let inputtt=document.getElementById('pepito3');
+    let pepito1=input.value;
+    let pepito2=inputt.value;
+    let pepito3=inputtt.value;
+    sessionStorage.setItem('pepito1',pepito1);
+    sessionStorage.setItem('pepito2',pepito2);
+    sessionStorage.setItem('pepito3',pepito3);
+}
+
+//como obtener el valor del session storage
+function obtenerValor (){
+    let pepito1=sessionStorage.getItem('pepito1');
+    let pepito2=sessionStorage.getItem('pepito2');
+    let pepito3=sessionStorage.getItem('pepito3');
+    console.log(pepito1,pepito2,pepito3);
+}
+
+
+
+
+
+
+
+
